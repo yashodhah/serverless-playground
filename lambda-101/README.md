02fabecb0d7703ff00e9844ea2121511ad584f54
yashodhah <y.dominatz@gmail.com>
2023-01-15 19:36:47 +0530

    lambda automated rollbacks

--- lambda-101/README.md
+++ lambda-101/README.md
@@ -1,5 +1,13 @@
+* Exporting variables
+
 API_URL=$(aws cloudformation describe-stacks --stack-name sam-app --query 'Stacks[0].Outputs[?OutputKey==`HelloWorldApi`].OutputValue' --output text)
 LAMBDA_ARN=$(aws cloudformation describe-stacks --stack-name sam-app --query 'Stacks[0].Outputs[?OutputKey==`HelloWorldFunction`].OutputValue' --output text)
 echo API_URL=$API_URL
 echo LAMBDA_ARN=$LAMBDA_ARN
 
+
+* Artilery Testing
+
+  API_URL=$(aws cloudformation describe-stacks --stack-name sam-app --query 'Stacks[0].Outputs[?OutputKey==`HelloWorldApi`].OutputValue' --output text)
+
+  artillery run --target $API_URL loadtest-canary.yaml
--- lambda-101/hello_world/app.py
+++ lambda-101/hello_world/app.py
@@ -1,12 +1,26 @@
 import json
-import time
+import random
 
-# Wait for 1 second to create an artificially long cold start
-time.sleep(1)
+
+# def lambda_handler(event, context):
+#
+#     # Return a response to the API Gateway
+#     return {
+#         "statusCode": 200,
+#         "body": json.dumps({
+#             "message": "hello from version 2"
+#         }),
+#     }
+
+
 
 def lambda_handler(event, context):
-    # Sleep for 100ms to simulate work
-    time.sleep(0.1)
+
+    # Pick a number between 0 and 200.
+    # If the value is zero, raise an Exception.
+    val = random.randrange(0, 200)
+    if val == 0:
+        raise Exception("something went wrong!")
 
     # Return a response to the API Gateway
     return {
